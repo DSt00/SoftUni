b6a1ac2f41a5664cc2c671ce7e1ef886a21800ff
stoyanov <dstoyanov16@gmail.com>
2020-11-25 20:39:36 +0200

    from Code

--- 02.Python Fundamentals/07.Exam Prep (Mid) 2/01. Computer Store.py
+++ 02.Python Fundamentals/07.Exam Prep (Mid) 2/01. Computer Store.py
@@ -1,20 +1,31 @@
-
+flag = False
 total = 0
-
-
-
 while True:
     price = (input())
     if price == "special" or price == "regular":
         break
 
+    flag = True
     price = float(price)
     if price < 0:
-        print("Invalid order!")
+        print("Invalid price!")
     elif price == 0:
         print('Invalid order!')
     else:
         total += price
 
-print(price)
-#customer_type =
+
+customer_type = price
+after_taxes = total * 1.2
+taxes = after_taxes - total
+if customer_type == "special":
+    after_taxes *= 0.9
+
+if flag:
+    print(f"Congratulations you've just bought a new computer!\n\
+    Price without taxes: {total:.2f}$\n\
+    Taxes: {taxes:.2f}$\n\
+    -----------\n\
+    Total price: {after_taxes:.2f}$")
+else:
+    print('Invalid order!')
